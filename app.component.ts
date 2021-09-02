0bd2a856d204b7a17e6658dc8a26a106c4519526
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-09-02 13:44:42 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  // Assign String Value
+  TxtId: string ="TxtFirstName";
+
+  //Assign Boolean Value
+  isDisable=true;
+
+}
